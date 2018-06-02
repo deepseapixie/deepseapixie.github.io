1c3ffe848be7637daf163bd7b1859530081a448a
Tykia Miller <kia.miller818@gmail.com>
2018-06-02 13:43:11 -0400

    Testing knockout.js install

--- js/Home/HomeJS.js
+++ js/Home/HomeJS.js
@@ -1,5 +1,6 @@
-var HomeViewModel = function () {
-    var message = ko.observable("Hello World! I'm Kia Miller!");
+
+function HomeViewModel() {
+    this.message = ko.observable("Hello World! I'm Kia Miller!");
 };
 
 ko.applyBindings(new HomeViewModel());
